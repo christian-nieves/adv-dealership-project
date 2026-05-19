c0efa850a1d72e4e2c8314667eb8ca10537d3af2
christian-nieves <thechristiannieves@gmail.com>
2026-05-19 14:19:39 -0400

    Started Sales Contract Class and added constructor

--- src/main/java/com/pluralsight/dealership/SalesContract.java
+++ src/main/java/com/pluralsight/dealership/SalesContract.java
@@ -1,4 +1,16 @@
 package com.pluralsight.dealership;
 
 public class SalesContract extends Contract {
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean financeOption;
+
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, double salesTaxAmount, double recordingFee, double processingFee, boolean financeOption) {
+        super(date, customerName, customerEmail, vehicle);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.financeOption = financeOption;
+    }
 }
