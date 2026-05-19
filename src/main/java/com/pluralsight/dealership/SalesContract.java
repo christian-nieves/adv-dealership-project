06e76156d6d5436878ae3b5579c272daaf2f933c
christian-nieves <thechristiannieves@gmail.com>
2026-05-19 14:28:39 -0400

    Fixed and update contrustor

--- src/main/java/com/pluralsight/dealership/SalesContract.java
+++ src/main/java/com/pluralsight/dealership/SalesContract.java
@@ -8,9 +8,9 @@ public class SalesContract extends Contract {
 
     public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicle, double salesTaxAmount, double recordingFee, double processingFee, boolean financeOption) {
         super(date, customerName, customerEmail, vehicle);
-        this.salesTaxAmount = salesTaxAmount;
-        this.recordingFee = recordingFee;
-        this.processingFee = processingFee;
+        this.salesTaxAmount = vehicle.getPrice() * 0.05;
+        this.recordingFee = 100;
+        this.processingFee = vehicle.getPrice() < 10000 ? 295.00 : 495.00; // if the vehicle price is under 10k the fee is 295, if not it's 495
         this.financeOption = financeOption;
     }
 }
